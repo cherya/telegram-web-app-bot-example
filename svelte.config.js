3e292730ce59a87c7782b93f360e64bfb2a326b2
cherya <ccherya@gmail.com>
2024-07-23 09:23:18 +0400

    path

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,7 @@ const config = {
       // fallback: 'src/index.html'
       runtime: 'nodejs18.x',
     }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/telegram-web-app-bot-example' : '',
-    }
+    paths: {}
 	},
   preprocess: vitePreprocess(),
 };
